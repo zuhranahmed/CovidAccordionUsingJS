61999590cdd32d14a0350558a2be94cacb8e6357
zuhranahmed <zuhranahmed@gmail.com>
2021-01-15 16:08:13 +0500

    a little change

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const openDetails = document.querySelectorAll(".heading");
 const openImg = document.querySelectorAll(".open");
+const closeImg = document.querySelectorAll(".close");
 
 for (let i = 0; i < openDetails.length; i++) {
   openDetails[i].addEventListener("click", function (e) {
@@ -7,8 +8,12 @@ for (let i = 0; i < openDetails.length; i++) {
     let details = openDetails[i].nextElementSibling;
     if (details.classList.contains("hide")) {
       details.classList.remove("hide");
+      openImg[i].classList.add("hide");
+      closeImg[i].classList.remove("hide");
     } else {
       details.classList.add("hide");
+      openImg[i].classList.remove("hide");
+      closeImg[i].classList.add("hide");
     }
   });
 }
